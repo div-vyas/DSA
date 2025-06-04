1afd0406fa5de70ee02f9d2dd59d52aa9f0dd2b8
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-06-04 09:07:58 +0530

    Time: 29 ms (60.47%) | Memory: 42.1 MB (81.24%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        // If sum is odd, can't split equally
+        if (totalSum % 2 != 0) {
+            return false;
+        }
+
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;  // 0 sum is always possible
+
+        for (int num : nums) {
+            for (int i = target; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
