65219f1d9f11bd8f43be810fc8635456988712ef
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-07-06 19:27:31 +0530

    Time: 141 ms (80.00%) | Memory: 73.6 MB (82.46%) - LeetSync

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.java
@@ -0,0 +1,36 @@
+class FindSumPairs {
+    int[] nums1, nums2;
+    Map<Integer, Integer> freqMap;
+
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        this.nums1 = nums1;
+        this.nums2 = nums2;
+        freqMap = new HashMap<>();
+        for (int num : nums2) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+    }
+    
+    public void add(int index, int val) {
+        int oldVal = nums2[index];
+        int newVal = oldVal + val;
+
+        // Update the frequency map
+        freqMap.put(oldVal, freqMap.get(oldVal) - 1);
+        if (freqMap.get(oldVal) == 0) {
+            freqMap.remove(oldVal);
+        }
+
+        nums2[index] = newVal;
+        freqMap.put(newVal, freqMap.getOrDefault(newVal, 0) + 1);
+    }
+    
+    public int count(int tot) {
+        int count = 0;
+        for (int a : nums1) {
+            int b = tot - a;
+            count += freqMap.getOrDefault(b, 0);
+        }
+        return count;
+    }
+}
