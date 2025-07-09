8ebbf608dd46a436b4f1430272f59875086592bc
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-07-09 20:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.94%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,31 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
+        // Optimal Dutch National Flag
+        int n = nums.length;
+        int lo = 0;
+        int mid = 0;
+        int hi = n-1;
 
-        //Brute
-
-        int count0 = 0, count1 = 0, count2 = 0;
-
-        // Count the number of 0s, 1s, and 2s
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                count0++;
-            else if (nums[i] == 1)
-                count1++;
-            else
-                count2++;
+        while(mid <= hi){
+            if(nums[mid] == 0){
+                swap(nums, lo, mid);
+                lo++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, hi);
+                hi--;
+            }
         }
+    }
 
-        // Overwrite the array with the correct counts
-        int index = 0;
-        for (int i = 0; i < count0; i++)
-            nums[index++] = 0;
-        for (int i = 0; i < count1; i++)
-            nums[index++] = 1;
-        for (int i = 0; i < count2; i++)
-            nums[index++] = 2;
-
-
-
-        
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
