b1d8af9abc7f52456e857afab8c778cc514f6295
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-07-15 07:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (80.65%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -2,10 +2,11 @@ class Solution {
     public List<Integer> getRow(int rowIndex) {
         ArrayList<Integer> ans = new ArrayList<>();
         long result = 1;
-        for(int i=0; i<=rowIndex; i++){
+        ans.add(1);
+        for(int i=1; i<=rowIndex; i++){
+            result = result * (rowIndex - i + 1);
+            result = result / (i);
             ans.add((int)result);
-            result = result * (rowIndex - i);
-            result = result / (i+1);
         }
         return ans;
     }
