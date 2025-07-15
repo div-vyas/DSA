c6bc411e57a376c7b60c8f816236822499206a2a
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-07-15 23:28:42 +0530

    Time: 16 ms (21.52%) | Memory: 49.2 MB (96.46%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,29 +1,46 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+
+        // // Brute - O(N^2)
+        // ArrayList<Integer> ls = new ArrayList<>();
+        // int n = nums.length;
+
+        // for(int i=0; i<n; i++){
+        //     if(ls.size() == 0 || ls.get(0) != nums[i]){
+        //         int cnt=0;
+        //         for(int j=0; j<n; j++){
+        //             if(nums[j] == nums[i]){
+        //                 cnt++;
+        //             }
+        //         }
+                
+        //         if(cnt > (n/3)){
+        //             ls.add(nums[i]);
+        //         }
+
+        //     }
+        //     if(ls.size() == 2){ 
+        //     break;
+        // }
+        // }
+        // return ls;
+
+
+        // Better - O(N)
         ArrayList<Integer> ls = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
 
-        for(int i=0; i<n; i++){
-            if(ls.size() == 0 || ls.get(0) != nums[i]){
-                int cnt=0;
-                for(int j=0; j<n; j++){
-                    if(nums[j] == nums[i]){
-                        cnt++;
-                    }
-                }
-                
-                if(cnt > (n/3)){
-                    ls.add(nums[i]);
-                }
+        int min = (int)(n/3) + 1;
 
+        for(int i =0; i<n; i++){
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value+1);
+
+            if(map.get(nums[i]) == min){
+                ls.add(nums[i]);
             }
-            if(ls.size() == 2){ 
-            break;
         }
-        }
-
-        
-
         return ls;
     }
 }
