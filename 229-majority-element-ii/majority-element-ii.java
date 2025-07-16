006365a85b372b9122751751ba291249e08dd03f
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-07-16 09:43:23 +0530

    Time: 2 ms (99.77%) | Memory: 50.4 MB (56.68%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,69 +1,24 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-
-        // // Brute - O(N^2)
-        // ArrayList<Integer> ls = new ArrayList<>();
-        // int n = nums.length;
-
-        // for(int i=0; i<n; i++){
-        //     if(ls.size() == 0 || ls.get(0) != nums[i]){
-        //         int cnt=0;
-        //         for(int j=0; j<n; j++){
-        //             if(nums[j] == nums[i]){
-        //                 cnt++;
-        //             }
-        //         }
-                
-        //         if(cnt > (n/3)){
-        //             ls.add(nums[i]);
-        //         }
-
-        //     }
-        //     if(ls.size() == 2){ 
-        //     break;
-        // }
-        // }
-        // return ls;
-
-
-        // // Better - O(N)
-        // ArrayList<Integer> ls = new ArrayList<>();
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // int n = nums.length;
-
-        // int min = (int)(n/3) + 1;
-
-        // for(int i =0; i<n; i++){
-        //     int value = map.getOrDefault(nums[i], 0);
-        //     map.put(nums[i], value+1);
-
-        //     if(map.get(nums[i]) == min){
-        //         ls.add(nums[i]);
-        //     }
-        // }
-        // return ls;
-
-        // Optimal - O(1) 
-
-        int ele1 = Integer.MIN_VALUE;
-        int ele2 = Integer.MIN_VALUE;
         int cnt1 = 0;
+        int el1 = Integer.MIN_VALUE;
         int cnt2 = 0;
+        int el2 = Integer.MIN_VALUE;
         int n = nums.length;
 
         for(int i=0; i<n; i++){
-            if(cnt1 == 0 && ele2 != nums[i]){
+            if(cnt1 == 0 && nums[i] != el2){
                 cnt1 = 1;
-                ele1 = nums[i];
+                el1 = nums[i];
             }
-            else if(cnt2 == 0 && ele1 != nums[i]){
+            else if(cnt2 == 0 && nums[i] != el1){
                 cnt2 = 1;
-                ele2 = nums[i];
+                el2 = nums[i];
             }
-            else if(nums[i] == ele1){
+            else if(nums[i] == el1){
                 cnt1++;
             }
-            else if(nums[i] == ele2){
+            else if(nums[i] == el2){
                 cnt2++;
             }
             else{
@@ -74,21 +29,22 @@ class Solution {
 
         ArrayList<Integer> ls = new ArrayList<>();
         int min = (int)(n/3) + 1;
-        cnt1 = 0;
-        cnt2 = 0;
+        cnt1 = 0; cnt2 = 0;
         for(int i=0; i<n; i++){
-            if(nums[i] == ele1){
+            if(nums[i] == el1){
                 cnt1++;
             }
-            if(nums[i] == ele2){
+            if(nums[i] == el2){
                 cnt2++;
             }
         }
-        if(cnt1 >= min)
-            ls.add(ele1);
-        if(cnt2 >= min)
-            ls.add(ele2);
-    
+        if(cnt1 >= min){
+            ls.add(el1);
+        }
+        if(cnt2 >= min){
+            ls.add(el2);
+        }
+
         return ls;
     }
 }
