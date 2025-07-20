9ed4533f03f0d1de5b0dda492e6ce2ff74fb7609
div-vyas <95860276+div-vyas@users.noreply.github.com>
2025-07-20 14:47:28 +0530

    Time: 27 ms (57.23%) | Memory: 57.6 MB (10.92%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int start, int end) {
+        if (start >= end) return;
+
+        int mid = start + (end - start) / 2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+        merge(nums, start, mid, end);
+    }
+
+    private void merge(int[] nums, int start, int mid, int end) {
+        int[] temp = new int[end - start + 1];
+        int i = start, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= end) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= end) temp[k++] = nums[j++];
+
+        for (int t = 0; t < temp.length; t++) {
+            nums[start + t] = temp[t];
+        }
+    }
+}
